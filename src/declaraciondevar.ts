a31996f089da4f5f5dfee6b8f20ef9912190c211
yamiladib <104107990+yamiladib@users.noreply.github.com>
2022-04-26 17:41:16 -0300

    variablesdeclaradas

---
+++ src/declaraciondevar.ts
@@ -0,0 +1,22 @@
+let paso1: string = "este es el paso1";
+let paso2: string = "este es el paso2";
+let paso3: string = "este es el paso3";
+let paso4: string = "este es el paso4";
+let paso5: string = "este es el paso5";
+let paso6: string = "este es el paso6";
+let paso7: string = "este es el paso7";
+let paso8: string = "este es el paso8";
+let paso9: string = "este es el paso9";
+let paso10: string = "este es el paso10";
+
+console.log ("preparar coulant de chocolate")
+console.log (paso1 "conseguir los ingredientes");
+console.log (paso2 "Mezcla la harina con el cacao amargo y tamiza");
+console.log (paso3 "Funde el chocolate a baño María o al microondas y una vez fundido incorpora la mantequilla a punto pomada. Continúa removiendo con unas varillas hasta derretir");
+console.log (paso4 "En otro recipiente coloca el azúcar junto con los huevos y bate hasta obtener una preparación cremosa y amarillenta");
+console.log (paso5 "Agrega el chocolate fundido templado y el extracto de vainilla y sigue batiendo hasta que se incorporen todos los ingredientes");
+console.log (paso6 "Finalmente, agrega la harina con el cacao amargo y bate una vez más hasta obtener un preparado uniforme");
+console.log (paso7 "Vierte en los moldes que hayas elegido hasta llegar casi al tope y deja reposar durante 120 minutos en el congelador");
+console.log (paso8 "Una vez que hayan transcurrido los 120 minutos, precalienta el horno a 200º C con calor arriba y abajo");
+console.log (paso9 "Trascurridos los 5 minutos de encendido del horno, baja la temperatura a 180º C y hornea durante 12-15 minutos);
+console.log (paso10 "Finalizada la cocción del coulant, deja enfriar durante algunos minutos y desmolda si se prefiere polvorea con azúcar glas o cacao amargo);
