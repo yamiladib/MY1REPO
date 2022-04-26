ebc51bccd3a80c05cf0fdbdaa77d740939cfc5d9
yamiladib <104107990+yamiladib@users.noreply.github.com>
2022-04-26 18:05:43 -0300

    ingresodedatos

--- src/ingresardato.ts
+++ src/ingresardato.ts
@@ -3,7 +3,9 @@ let dato = document.getElementById("dato");
 let rotulo = document.getElementById("rotulo");
 
 btnEnv.addEventListener("click", () => {
-  // TU CODIGO VA AQUI, POR EJEMPLO:
+  // TU CODIGO VA AQUI
   console.log("el dato ingresado es ", dato.value);
   //}
 });
+
+rotulo.innerHTML = "ingrese dato";
