a18d367f91be4787139843d8072ea4e048ed380a
yamiladib <104107990+yamiladib@users.noreply.github.com>
2022-05-02 20:38:30 -0300

    EJERCICIO

--- PROGRAMACION/src/ejercicio1.ts
+++ PROGRAMACION/src/ejercicio1.ts
@@ -7,9 +7,9 @@ let rotulo1 = document.getElementById("rotulo1");
 let rotulo2 = document.getElementById("rotulo2");
 let rotulo3 = document.getElementById("rotulo3");
 
-rotulo1.innerHTML = "Ingrese 1er numero ";
-rotulo2.innerHTML = "Ingrese 2do numero";
-rotulo3.innerHTML = "Ingrese 3er numero ";
+rotulo1.innerHTML = "Ingrese1ernumero";
+rotulo2.innerHTML = "Ingrese2donumero";
+rotulo3.innerHTML = "Ingrese3ernumero";
 
 
 btnEnviar.addEventListener("click", () => {
@@ -18,7 +18,11 @@ btnEnviar.addEventListener("click", () => {
   let numero3: number = dato3.value; 
    
   if (numero1 > numero2 && numero1>numero3){
-    console.log  }
+    console.log ("el 1er numero es el mayor");  
+  } else if (numero2 > numero1 && numero2>numero3){ 
+   console.log ("el 2do numero es el mayor");
+} else if  (numero3>numero1 && numero3>numero2) {
+  console.log ("el 3er numero es el mayor")}
   
   }
 });
