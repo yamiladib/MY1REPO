c3aae06eeca1f4e354b835e9cfae05700bceb505
yamiladib <104107990+yamiladib@users.noreply.github.com>
2022-05-08 20:55:09 -0300

    acomodo

--- PROGRAMACION/src/ejercicio1.ts
+++ PROGRAMACION/src/ejercicio1.ts
@@ -1,6 +1,6 @@
+let btnEnv = document.getElementById("btnEnviar");
 let dato1 = document.getElementById("dato1");
 let dato2 = document.getElementById("dato2");
-let dato3 = document.getElementById("dato3");
 
 let btnEnviar = document.getElementById("enviar");
 let rotulo1 = document.getElementById("rotulo1");
