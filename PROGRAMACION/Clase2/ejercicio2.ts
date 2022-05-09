154a4321d609eb6e87ba8c9a42b673ecd3264cdc
yamiladib <104107990+yamiladib@users.noreply.github.com>
2022-05-09 12:24:39 -0300

    acomodo

--- PROGRAMACION/Clase2/ejercicio2.ts
+++ PROGRAMACION/Clase2/ejercicio2.ts
@@ -1,6 +1,18 @@
 //Calcular area de um rectangulo
-let base = 5;
-let altura = 10;
-let resultado = base * altura;
+//let base = 5;
+//let altura = 10;
+//let resultado = base * altura;
 
-console.log = resultado;
+//console.log = resultado;
+
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = document.getElementById("rotulo");
+
+btnEnv.addEventListener("click", () => {
+  // TU CODIGO VA AQUI
+  console.log("el dato ingresado es ", dato.value);
+  //}
+});
+
+rotulo.innerHTML = "ingrese dato";
