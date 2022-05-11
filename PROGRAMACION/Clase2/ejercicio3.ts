aac437c2d25216f7b344dec5d47b6921595b5600
yamiladib <104107990+yamiladib@users.noreply.github.com>
2022-05-10 23:54:55 -0300

    acomodo

--- PROGRAMACION/Clase2/ejercicio3.ts
+++ PROGRAMACION/Clase2/ejercicio3.ts
@@ -1,5 +1,5 @@
 let btnEnv = document.getElementById("btnEnviar");
-let dato = document.getElementById("dato");
+let dato = document.forms[0].dato);
 let rotulo = document.getElementById("rotulo");
 
 btnEnv.addEventListener("click", () => {
