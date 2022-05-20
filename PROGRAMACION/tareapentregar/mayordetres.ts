d1fbf622af936f815ac0a3d4abd3a1b9aa4379e4
yamiladib <104107990+yamiladib@users.noreply.github.com>
2022-05-20 18:56:54 -0300

    acomodo

--- PROGRAMACION/tareapentregar/mayordetres.ts
+++ PROGRAMACION/tareapentregar/mayordetres.ts
@@ -1,16 +1,11 @@
+let rotulo = document.getElementById("rotulo");
+let rotulo1 = document.getElementById("rotulo1");
+let rotulo2 = document.getElementById("rotulo2");
+
 let btnEnv = document.getElementById("btnEnviar");
 let dato1 = document.getElementById("dato1");
 let dato2 = document.getElementById("dato2");
-
-let btnEnviar = document.getElementById("enviar");
-let rotulo1 = document.getElementById("rotulo1");
-let rotulo2 = document.getElementById("rotulo2");
-let rotulo3 = document.getElementById("rotulo3");
-
-rotulo1.innerHTML = "Ingrese1ernumero";
-rotulo2.innerHTML = "Ingrese2donumero";
-rotulo3.innerHTML = "Ingrese3ernumero";
-
+let dato3 = document.getElementById("dato3");
 
 btnEnviar.addEventListener("click", () => {
   let numero1: number = dato1.value ;
@@ -18,11 +13,14 @@ btnEnviar.addEventListener("click", () => {
   let numero3: number = dato3.value; 
    
   if (numero1 > numero2 && numero1>numero3){
-    console.log ("el 1er numero es el mayor");  
+    console.log ("el numero1 numero es el mayor");  
   } else if (numero2 > numero1 && numero2>numero3){ 
-   console.log ("el 2do numero es el mayor");
+   console.log ("el numero2 numero es el mayor");
 } else if  (numero3>numero1 && numero3>numero2) {
-  console.log ("el 3er numero es el mayor")}
+  console.log ("el numero3 numero es el mayor")}
   
   }
 });
+rotulo1.innerHTML = "Ingrese1ernumero";
+rotulo2.innerHTML = "Ingrese2donumero";
+rotulo3.innerHTML = "Ingrese3ernumero";
