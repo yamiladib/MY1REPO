050483778f95cd6e8654dae8787cded27245bb9a
yamiladib <104107990+yamiladib@users.noreply.github.com>
2022-05-25 21:12:08 -0300

    acomodo

--- PROGRAMACION/Clase2/src/ejercicio2.ts
+++
@@ -1,6 +0,0 @@
-//Calcular area de um rectangulo
-let base = 5;
-let altura = 10;
-let resultado = base * altura;
-
-console.log = resultado;
---
+++ PROGRAMACION/Clase5/src/saludar5veces.ts
@@ -0,0 +1,5 @@
+console.log ("hola");
+console.log ("hola");
+console.log ("hola");
+console.log ("hola");
+console.log ("hola");
